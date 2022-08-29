0729553a483afb0e6e92d2af705c9b62ab31272b
mad9639 <madhurs06@gmail.com>
2022-08-29 13:35:51 +0530

    Initail Commit

---
+++ app.js
@@ -0,0 +1,116 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lod = require("lodash")
+const mongoose = require("mongoose");
+
+
+
+const homeStartingContent = "Hi There, Welcome to my blogspot. I come and write here when I'm stressed out. I read somewhere, \" If you want clarity, Write \" ";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-madhur:test123@cluster0.kumfynt.mongodb.net/blogDb")
+
+
+const blogSchema = {
+  title : String,
+  content : String
+}
+
+const Blog = mongoose.model("Blog" , blogSchema)
+
+app.get("/", function(req, res){
+
+  Blog.find({}, function(err , posts){
+      res.render("home" , {
+        startingContent : homeStartingContent,
+        posts : posts
+      });
+
+    })
+});
+
+
+
+app.get("/about", function(req, res){
+
+  res.render("about" , {
+    about : aboutContent
+
+  });
+
+})
+app.get("/contact", function(req, res){
+
+  res.render("contact" , {
+    contact : contactContent
+
+  });
+
+})
+
+
+app.get("/compose", function(req, res){
+
+ res.render("compose" );
+
+})
+
+app.get("/posts/:postID", function(req,res){
+  
+  const reqID = req.params.postID;
+  
+  Blog.findOne({_id: reqID}, function(err, post){
+
+    res.render("post", {
+    
+    title: post.title,
+    
+    content: post.content
+    
+    });
+    
+    });
+    
+    
+})
+
+app.post("/compose", function(req, res){
+
+  const post = new Blog({
+    title : req.body.posttitle ,
+    content : req.body.postbody
+  })
+
+  post.save(function(err){
+
+    if (!err){
+    
+    res.redirect("/");
+    
+    }
+    
+    });
+  
+  
+ })
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
